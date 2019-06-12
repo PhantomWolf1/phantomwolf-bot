d91792c6a4754ec614074d5f4def63e08aaf3116
Austin Gallippo <phantomwolf154@gmail.com>
2019-06-12 02:26:41 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const fs = require("fs");
+var prefix = "!"
+bot.commands = new Discord.Collection()
+
+fs.readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on('ready', () => {
+  bot.user.setStatus("Watching over PhantomWolf Dev server.")
+})
+
+  bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(prefix)) return;
+  if (cmd) {
+    cmd.run(bot, message, args, Discord)
+    console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+    
+  }
+})
+
+bot.login(process.env.Token)
